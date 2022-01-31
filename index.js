72961d60c73dcc536d95d658a2ccffd560946612
stephen talley <stvlley.amzn@gmail.com>
2022-01-31 15:42:16 -0600

    commit tree 9b345f5a21f3a715d6359d61cfcc0d10c89ecb19

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(value){
+  // free sample if ride<= 400ft
+  if (value <= 400) {
+    return "This one is on me!";
+  } else if (value >= 2001 && value <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else  {
+    return("No can do.");
+  }
+  
+};
+  
+function ternaryCheckCity(city){
+return city == "NYC" ? "Ok, sounds good.": "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+let tip;
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+  case 'generous':
+  return 'Thank you so much.'
+  break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  case 'not as generous':
+  return 'Thank you.'
+  break;
+
+  default:
+    return "Bye."
+}};
